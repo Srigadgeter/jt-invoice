5ff78a102bbc7c2003725fd931ee83ad6c9f3d9c
Srigadgeter <srigadgets@gmail.com>
2023-11-19 17:31:22 +0530

    implemented & added appSlice to the store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+import appReducer from "./slices/appSlice";
 import invoicesReducer from "./slices/invoicesSlice";
 
 const store = configureStore({
   reducer: {
+    app: appReducer,
     invoices: invoicesReducer
   }
 });
