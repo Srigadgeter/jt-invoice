bd96c4ab20df090a347486364b088552b81ea421
Srigadgeter <srigadgets@gmail.com>
2023-11-19 17:30:07 +0530

    added constants

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+// pages info
+export const PAGE_INFO = {
+  INVOICES: {
+    title: "Invoices",
+    description: "List of all bills"
+  }
+};
+
+// theme constants
+export const DEFAULT_LIGHT = "DEFAULT_LIGHT";
+export const DEFAULT_DARK = "DEFAULT_DARK";
