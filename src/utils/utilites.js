034c4bc21a9337508ff75dfb7ac0046684d70a9d
Srigadgeter <srigadgets@gmail.com>
2024-07-11 19:50:56 +0530

    added indian currency formatter util fn

--- src/utils/utilites.js
+++ src/utils/utilites.js
@@ -1,3 +1,13 @@
+/* eslint-disable no-restricted-globals */
 export const isMobile = () => window.innerWidth <= 768;
 
 export const trimString = (text) => (typeof text === "string" ? text.trim() : text);
+
+export const indianCurrencyFormatter = (number) => {
+  if (!isNaN(number))
+    return new Intl.NumberFormat("en-IN", {
+      style: "currency",
+      currency: "INR"
+    }).format(number);
+  throw new Error(`'${number}' is not a number`);
+};
