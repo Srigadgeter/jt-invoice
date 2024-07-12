5aa98ee56d8f6fbf230b0ef99c75d6430050e809
Srigadgeter <srigadgets@gmail.com>
2024-07-12 09:15:21 +0530

    updated theme config

--- src/theme/default_dark.js
+++ src/theme/default_dark.js
@@ -2,10 +2,14 @@ const Options = {
   palette: {
     mode: "dark",
     common: {
-      pink: "#f574b9"
+      pink: "#f574b9",
+      success: "#D6F2E3",
+      error: "#FEE2E2"
     },
     background: {
-      custom: "#121212"
+      custom: "#121212",
+      success: "#315345",
+      error: "#6c2f2f"
     }
   }
 };
--- src/theme/default_light.js
+++ src/theme/default_light.js
@@ -2,7 +2,13 @@ const Options = {
   palette: {
     mode: "light",
     common: {
-      pink: "#f574b9"
+      pink: "#f574b9",
+      success: "#315345",
+      error: "#991B1B"
+    },
+    background: {
+      success: "#D6F2E3",
+      error: "#FEE2E2"
     }
   }
 };
