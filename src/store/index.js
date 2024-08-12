5926864074a92736ea869fe3761ca651ee52966b
Srigadgeter <srigadgets@gmail.com>
2024-08-12 18:19:41 +0530

    added ProductsReducer to the store config

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import appReducer from "./slices/appSlice";
 import invoicesReducer from "./slices/invoicesSlice";
+import productsReducer from "./slices/productsSlice";
 
 const store = configureStore({
   reducer: {
     app: appReducer,
-    invoices: invoicesReducer
+    invoices: invoicesReducer,
+    products: productsReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
