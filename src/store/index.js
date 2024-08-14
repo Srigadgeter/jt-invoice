c23e7e03b02f357b0bef5066759fd4d020fbad7f
Srigadgeter <srigadgets@gmail.com>
2024-08-14 22:34:29 +0530

    implemented reducer for customers data

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import appReducer from "./slices/appSlice";
 import invoicesReducer from "./slices/invoicesSlice";
 import productsReducer from "./slices/productsSlice";
+import customersReducer from "./slices/customersSlice";
 
 const store = configureStore({
   reducer: {
     app: appReducer,
     invoices: invoicesReducer,
-    products: productsReducer
+    products: productsReducer,
+    customers: customersReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
