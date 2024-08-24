6388b59e3ac96e46c1791a1225d1e5924d3741c2
Srigadgeter <srigadgets@gmail.com>
2024-08-24 11:47:51 +0530

    implemented new Loader component

---
+++ src/components/common/Loader.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const styles = {
+  box: (height) => ({
+    width: "100%",
+    display: "flex",
+    position: "absolute",
+    alignItems: "center",
+    justifyContent: "center",
+    height: height ?? "100%"
+  })
+};
+
+const Loader = ({ height = null }) => (
+  <Box sx={styles.box(height)}>
+    <CircularProgress />
+  </Box>
+);
+
+export default Loader;
