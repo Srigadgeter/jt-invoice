2d38edbed338f323f61572db2c9993dd191064d6
Srigadgeter <srigadgets@gmail.com>
2024-08-24 11:54:40 +0530

    used loader in app.js for fallback ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 // external imports
 import React, { Suspense } from "react";
-import CircularProgress from "@mui/material/CircularProgress";
 
 import AllRoutes from "routes/AllRoutes";
+import Loader from "components/common/Loader";
 import ErrorBoundary from "components/common/ErrorBoundary";
 
 // stylesheets
 import "./App.css";
 
 const App = () => (
-  <Suspense
-    fallback={<CircularProgress />} // based on theme, the CircularProgress component should be updated
-  >
+  <Suspense fallback={<Loader />}>
     <ErrorBoundary>
       <AllRoutes />
     </ErrorBoundary>
