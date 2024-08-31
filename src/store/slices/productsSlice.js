8510598cadf581fd67a568baa474c65360037d6a
Srigadgeter <srigadgets@gmail.com>
2024-08-31 14:05:47 +0530

    added reducer fns for products

--- src/store/slices/productsSlice.js
+++ src/store/slices/productsSlice.js
@@ -1,13 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// import { MODES } from "utils/constants";
-
 const initialState = {
-  products: [],
-  selectedProductInitialValue: {},
-  selectedProduct: {},
-  newProduct: {},
-  pageMode: ""
+  products: []
 };
 
 const productsSlice = createSlice({
@@ -16,10 +10,25 @@ const productsSlice = createSlice({
   reducers: {
     setProducts: (state, action) => {
       state.products = action?.payload;
+    },
+    addProduct: (state, action) => {
+      const currentProducts = [...state.products];
+      state.products = [...currentProducts, { ...action?.payload }];
+    },
+    editProduct: (state, action) => {
+      const modifiedProducts = state?.products?.map((item) => {
+        if (item?.id === action?.payload?.id) return { ...action?.payload };
+        return item;
+      });
+      state.products = modifiedProducts;
+    },
+    deleteProduct: (state, action) => {
+      const filteredProducts = state?.products?.filter((item) => item?.id !== action?.payload?.id);
+      state.products = filteredProducts;
     }
   }
 });
 
-export const { setProducts } = productsSlice.actions;
+export const { setProducts, addProduct, editProduct, deleteProduct } = productsSlice.actions;
 
 export default productsSlice.reducer;
