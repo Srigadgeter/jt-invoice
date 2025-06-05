63dbf913690d143d636788e1ede9161ebeeee2df
Srigadgeter <srigadgets@gmail.com>
2025-06-05 13:00:44 +0530

    added few theme colors

--- src/theme/default_dark.js
+++ src/theme/default_dark.js
@@ -1,10 +1,26 @@
 const Options = {
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200
+    }
+  },
   palette: {
     mode: "dark",
     common: {
       pink: "#f574b9",
+      blue1: "#bbdefb60",
+      blue2: "#42a5f560",
+      blue3: "#1976d290",
+      blue4: "#bbdefb",
+      blue5: "#42a5f5",
+      blue6: "#1976d2",
       success: "#D6F2E3",
       error: "#FEE2E2",
+      success1: "#315345",
+      error1: "#991B1B",
       shadowColor: "#c0bfbf59",
       dividerBorder: "rgba(0, 0, 0, 0.12)",
       tableBorder: "rgba(224, 224, 224, 1)"
--- src/theme/default_light.js
+++ src/theme/default_light.js
@@ -1,10 +1,26 @@
 const Options = {
+  breakpoints: {
+    values: {
+      mobile: 0,
+      tablet: 640,
+      laptop: 1024,
+      desktop: 1200
+    }
+  },
   palette: {
     mode: "light",
     common: {
       pink: "#f574b9",
+      blue1: "#bbdefb60",
+      blue2: "#42a5f560",
+      blue3: "#1976d290",
+      blue4: "#bbdefb",
+      blue5: "#42a5f5",
+      blue6: "#1976d2",
       success: "#315345",
       error: "#991B1B",
+      success1: "#315345",
+      error1: "#991B1B",
       shadowColor: "#00000059",
       dividerBorder: "rgba(0, 0, 0, 0.12)",
       tableBorder: "rgba(224, 224, 224, 1)"
